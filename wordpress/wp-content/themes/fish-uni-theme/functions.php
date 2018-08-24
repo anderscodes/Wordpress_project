e7e59aa467f33f49a8fc944f487c6ffeae3505fb
anderscodes <anders.j.persson@hotmail.com>
2018-08-24 22:16:22 +0100

    styling added to footer and header

--- wordpress/wp-content/themes/fish-uni-theme/functions.php
+++ wordpress/wp-content/themes/fish-uni-theme/functions.php
@@ -1,6 +1,7 @@
 <?php
 
 function uni_files() {
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('uni_main_style', get_stylesheet_uri());
 }
 
