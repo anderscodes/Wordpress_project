3911e30788a1130214620f92b3b0ec217cbe72d0
anderscodes <anders.j.persson@hotmail.com>
2018-08-28 20:46:42 +0100

    Post title re-styled

--- wordpress/wp-content/themes/fish-uni-theme/index.php
+++ wordpress/wp-content/themes/fish-uni-theme/index.php
@@ -17,7 +17,7 @@ get_header(); ?>
   while(have_posts()) {
     the_post(); ?>
     <div class="post-item">
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <div class="metabox">
         <p>Posted by Anders on 26082018 in News</p>
       </div>
