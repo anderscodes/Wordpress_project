e508956a590e7b966c6fb37b47a91864aac03750
anderscodes <anders.j.persson@hotmail.com>
2018-08-28 18:33:45 +0100

    continue reading button added

--- wordpress/wp-content/themes/fish-uni-theme/index.php
+++ wordpress/wp-content/themes/fish-uni-theme/index.php
@@ -18,6 +18,13 @@ get_header(); ?>
     the_post(); ?>
     <div class="post-item">
       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <div class="metabox">
+        <p>Posted by Anders on 26082018 in News</p>
+      </div>
+      <div class="generic-content">
+        <?php the_excerpt(); ?>
+        <p><a class='btn btn--blue' href="<?php the_permalink(); ?>"> Continue reading &raquo;</a></p>
+      </div>
     </div>
   <?php }
  ?>
