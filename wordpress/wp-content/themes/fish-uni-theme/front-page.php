041fa83f8deeed58a43a470b6cd9a1f19a6e1204
anderscodes <anders.j.persson@hotmail.com>
2018-08-30 20:48:12 +0100

    permalink added to title of hompageposts object

--- wordpress/wp-content/themes/fish-uni-theme/front-page.php
+++ wordpress/wp-content/themes/fish-uni-theme/front-page.php
@@ -56,7 +56,7 @@
                <span class="event-summary__day">20</span>
              </a>
              <div class="event-summary__content">
-               <h5 class="event-summary__title headline headline--tiny"><a href="#" <?php the_title(); ?></a></h5>
+               <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo the_permalink(); ?>" <?php the_title(); ?></a></h5>
                <p> <?php the_title(); ?> <a href="#" class="nu gray">Read more</a></p>
              </div>
            </div>
