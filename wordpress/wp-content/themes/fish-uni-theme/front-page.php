4fc4c0ed1a5456e29ec7f653f10c0b98971c7ff9
anderscodes <anders.j.persson@hotmail.com>
2018-08-30 20:58:59 +0100

    link added to title of homepageposts

--- wordpress/wp-content/themes/fish-uni-theme/front-page.php
+++ wordpress/wp-content/themes/fish-uni-theme/front-page.php
@@ -56,7 +56,7 @@
                <span class="event-summary__day"><?php the_time('d'); ?></span>
              </a>
              <div class="event-summary__content">
-               <h5 class="event-summary__title headline headline--tiny"><a href="<?php echo the_permalink(); ?>" <?php the_title(); ?></a></h5>
+               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
                <p> <a href="<?php echo the_permalink(); ?>" class="nu gray">Read more</a></p>
              </div>
            </div>
